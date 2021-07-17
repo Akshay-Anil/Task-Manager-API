fad791712587d2e7112e5a629412a79cfab707d1
Akshay-Anil <akshayanil005@gmail.com>
2021-07-17 19:15:05 +0530

    second commit

--- src/email/account.js
+++ src/email/account.js
@@ -2,7 +2,7 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const sendWelcomeMail = (email,name)=>{
+const sendWelcomeMail = (email,name)=>{ 
 sgMail.send({
     to: email,
     from:'akshay_b180034cs@nitc.ac.in',
